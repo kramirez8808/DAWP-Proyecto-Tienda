bf80e8838ccaadd41e250cb8081c66dee3c80e40
Kevin R. <kramirez8808@gmail.com>
2024-02-06 20:52:05 -0600

    Commit 4: MVC Introducción - Semana 4

---
+++ src/main/java/dawp/tienda/controller/IndexController.java
@@ -0,0 +1,17 @@
+package dawp.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "index.html";
+    }
+    
+    
+}
