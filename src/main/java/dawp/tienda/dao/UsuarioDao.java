1395f74762686ded51020e4e4d79e4b27e9df58c
Kevin R. <kramirez8808@gmail.com>
2024-03-25 09:28:00 -0600

    Commit 9: Seguridad - Semana 10

---
+++ src/main/java/dawp/tienda/dao/UsuarioDao.java
@@ -0,0 +1,8 @@
+package dawp.tienda.dao;
+
+import dawp.tienda.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
+    Usuario findByUsername(String username);
+}
---
+++ src/main/java/dawp/tienda/service/UsuarioDetailsService.java
@@ -0,0 +1,8 @@
+package dawp.tienda.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UsuarioDetailsService {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+}
